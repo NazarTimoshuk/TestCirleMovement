40cc2ba36aa9fd972ff9123b74c163d0f1e9cd9d
Nazar <nazar.timoshuk@gmail.com>
2024-08-08 13:36:08 +0300

    init

---
+++ TestCircleMovement/Assets/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] TouchDetection touchDetection;
+
+    public float speed = 5f;
+
+    [SerializeField] Slider speedSlider;
+    void Start()
+    {
+        StartCoroutine(MoveCharacter());
+        OnSliderValueChanged();
+    }
+
+    IEnumerator MoveCharacter()
+    {
+        while (true)
+        {
+            while (touchDetection.GetQueueCount() > 0)
+            {
+                Vector3 targetPosition = touchDetection.DequeuePosition();
+                while (Vector3.Distance(transform.position, targetPosition) > 0.1f)
+                {
+                    transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+                    yield return null;
+                }
+                yield return null;
+            }
+            yield return null;
+        }
+    }
+
+    public void OnSliderValueChanged()
+    {
+        speed = speedSlider.value * 5;
+    }
+}
---
+++ TestCircleMovement/Assets/TouchDetection.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TouchDetection : MonoBehaviour
+{
+    private Camera mainCamera;
+
+    private Queue<Vector3> queuePosition = new Queue<Vector3>();
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                Vector3 touchPosition = touch.position;
+
+                Ray ray = mainCamera.ScreenPointToRay(touchPosition);
+
+                RaycastHit2D hit = Physics2D.GetRayIntersection(ray, Mathf.Infinity);
+                if (hit.collider != null && !EventSystem.current.IsPointerOverGameObject(touch.fingerId))
+                {
+                    queuePosition.Enqueue(hit.point);
+                }
+            }
+        }
+    }
+    public Vector3 DequeuePosition()
+    {
+        return queuePosition.Dequeue();
+    }
+    public int GetQueueCount()
+    {
+        return queuePosition.Count;
+    }
+}
